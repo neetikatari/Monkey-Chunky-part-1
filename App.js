efcb2eae08b2f1dbafe405cd44b7c5779a4d896b
neetikatari <katari2009@gmail.com>
2021-09-06 20:14:06 +0530

    changed the location of the logo

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import  React from 'react';
 import {
   Text,
   View,
@@ -106,6 +106,6 @@ const styles = StyleSheet.create({
   imageIcon: {
     width: 150,
     height: 150,
-    marginLeft: 95,
+    alignSelf:"center",
   }
 });
